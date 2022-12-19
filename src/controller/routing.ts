7ce0c2c7d29a75140d44d09f507620349c1d424c
Alexey Kuptsov <jerubbaal@mail.ru>
2022-12-19 18:58:02 +0600

    feat: add `setParams` function

--- src/controller/routing.ts
+++ src/controller/routing.ts
@@ -1,6 +1,8 @@
 import { components } from '../model/comp-factory';
 import { iComponent } from '../view/components/component';
-import { List } from '../view/entyties';
+import { List, QueryParams } from '../view/entyties';
+
+let queryParams: QueryParams = {} as QueryParams;
 
 export const route = (e: Event) => {
     const event: Event = e || window.event;
@@ -19,6 +21,23 @@ export const handleLocation = () => {
     const urlSearchParams = new URLSearchParams(window.location.search);
     const path = window.location.pathname;
     const route = (routeHandler[path] || routeHandler['404']) as iComponent;
-    const params = Object.fromEntries(urlSearchParams.entries());
-    route.render(document.body, params);
+    queryParams = Object.fromEntries(urlSearchParams.entries());
+    route.render(document.body, queryParams);
+};
+
+export const setParams = (nesParam: Partial<QueryParams>) => {
+    Object.assign(queryParams, nesParam);
+    Object.keys(queryParams).forEach((key) => {
+        if (!queryParams[key]) delete queryParams[key];
+    });
+
+    const urlSearchParams = new URLSearchParams();
+    for (const key in queryParams) {
+        urlSearchParams.append(key, queryParams[key]?.toString() ?? '');
+    }
+    window.location.search = '?' + urlSearchParams.toString();
+
+    const path = window.location.pathname;
+    const route = (routeHandler[path] || routeHandler['404']) as iComponent;
+    route.render(document.body, queryParams);
 };
