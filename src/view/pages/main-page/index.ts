29f0aa20ca7e663d557ff68a2daa26719e8b940d
Alexey Kuptsov <jerubbaal@mail.ru>
2022-12-19 15:43:23 +0600

    feat: change to local data with filter

--- src/view/pages/main-page/index.ts
+++ src/view/pages/main-page/index.ts
@@ -4,24 +4,24 @@ import { QueryParams } from '../../entyties';
 import { components } from '../../../model/comp-factory';
 
 export class MainShopPage implements iComponent {
-    async render(root: HTMLElement, params?: List<string>) {
-      const $header = document.createElement('header');
-      const $main = document.createElement('main');
-      $main.className = 'main'
+    render(root: HTMLElement, params?: QueryParams) {
+        const $header = document.createElement('header');
+        const $main = document.createElement('main');
+        $main.className = 'main';
 
-      const $filter = document.createElement('aside');
-      const $cardList = document.createElement('section');
-      $cardList.className = 'card-list'
-      const $footer = document.createElement('footer');
+        const $filter = document.createElement('aside');
+        const $cardList = document.createElement('section');
+        $cardList.className = 'card-list';
+        const $footer = document.createElement('footer');
 
-      components.getHeader().render($header);
-      components.getFilter().render($filter);
-      components.getCardList().render($cardList, params);
-      components.getFooter().render($footer);
+        components.getHeader().render($header);
+        components.getFilter().render($filter);
+        components.getCardList().render($cardList, params);
+        components.getFooter().render($footer);
 
-      $main.append($filter, $cardList);
-      root.append($header, $main, $footer);
+        $main.append($filter, $cardList);
+        root.append($header, $main, $footer);
 
-      console.log('CardList rendered! Params:', params);
-  }
+        console.log('CardList rendered! Params:', params);
+    }
 }
