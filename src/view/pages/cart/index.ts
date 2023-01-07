18d09f018c6a3a89e5ebdc3d6d1321d4ffc216a1
Alexey Kuptsov <jerubbaal@mail.ru>
2023-01-07 16:00:07 +0600

    fix: any type

--- src/view/pages/cart/index.ts
+++ src/view/pages/cart/index.ts
@@ -6,6 +6,7 @@ import * as cartList from '../../pages/cart/cart.funcs';
 import { ProductInCart } from '../../components/product-in-card';
 import * as promos from './promos';
 import { ModalWindow } from '../../components/modal-window';
+import { callbackFunc } from '../../entyties';
 
 export class Cart implements iComponent {
     currentPage = 1;
@@ -174,7 +175,7 @@ export class Cart implements iComponent {
             };
             inputHandler();
             $promo__input.oninput = inputHandler;
-            $summary__buyButton.onclick = () => new ModalWindow().render(() => cartList.clearCart());
+            $summary__buyButton.onclick = () => new ModalWindow().render((() => cartList.clearCart()) as callbackFunc);
         } else {
             // Empty Cart
             const $emptyCart = new Constructor('div', 'empty-cart').create();
